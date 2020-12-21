2dd07233043c757d64b022ad741170571898f486
Blake Johnson <blake.k.johnson.4@gmail.com>
2020-12-21 12:26:29 -0500

    created bare minimum

--- main.js
+++ main.js
@@ -3,12 +3,15 @@
  * 12-20-20: Blake Johnson
  */
 
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const { readdir } = require('fs');
+const { join } = require('path');
+require('dotenv').config();
 
 function createWindow() {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 480,
+        height: 800,
         webPreferences: {
             nodeIntegration: true,
         },
@@ -31,3 +34,15 @@ app.on('activate', () => {
     }
 });
 
+const fileRegex = /^[^.]*\.(gif|png|jpe?g)$/;
+
+const filterFiles = file => fileRegex.test(file);
+const addPath = file => join(process.env.MOUNT_POINT, file);
+
+ipcMain.on('files', (event, arg) => {
+    readdir(process.env.MOUNT_POINT, (err, files) => {
+        let filteredFiles = files.filter(filterFiles);
+        let fullFiles = filteredFiles.map(addPath);
+        event.reply('files', fullFiles);
+    });
+});
---
+++ renderer.js
@@ -0,0 +1,39 @@
+const { ipcRenderer } = require('electron');
+
+const imgEl = document.getElementById('current-img');
+
+var files = [];
+var currentImage = 0;
+
+ipcRenderer.send('files');
+
+ipcRenderer.on('files', (event, incomingFiles) => {
+	incomingFiles.forEach((file) => {
+		files.push(file);
+	});
+	updateImage();
+});
+
+const updateImage = () => {
+	if (currentImage >= files.length || currentImage < 0) {
+		imgEl.src = 'placeholder.png';
+		return;
+	}
+
+	imgEl.src = `file://${files[currentImage]}`;
+};
+
+const nextImage = () => {
+	currentImage++;
+	if (currentImage >= files.length) currentImage = 0;
+	updateImage();
+};
+
+const previousImage = () => {
+	currentImage--;
+	if (currentImage < 0) currentImage = files.length - 1;
+	updateImage();
+};
+
+document.getElementById('next').addEventListener('click', nextImage);
+document.getElementById('previous').addEventListener('click', previousImage);
