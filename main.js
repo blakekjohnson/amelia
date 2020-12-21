da4c35c7773291ca2faec1e275cd33ed84c4894a
Blake Johnson <blake.k.johnson.4@gmail.com>
2020-12-20 19:52:45 -0500

    setup

---
+++ main.js
@@ -0,0 +1,33 @@
+/**
+ * Main process
+ * 12-20-20: Blake Johnson
+ */
+
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
